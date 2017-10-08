4c7b62dff8c2c27b52bafde02eca393e56a6ae0a
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-10-08 19:23:16 +0300

    Added helper for response

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::MatchesController < ApplicationController
+  include Response
+  include ExceptionHandler
   def index
     @matches = Match.all
-
-    render json: @matches
+    json_response @matches
   end
 
   def show
     @match = Match.find_by(match_id: params[:match_id])
-
-    render json: @matches
+    json_response @matches
   end
 end
