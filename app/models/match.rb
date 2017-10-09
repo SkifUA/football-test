23f8fe8574a794937f75af94025ea41a284b9184
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-10-09 09:03:37 +0300

    Updated method fot update matches

--- app/models/match.rb
+++ app/models/match.rb
@@ -7,16 +7,16 @@ class Match < ApplicationRecord
 
   def self.update_matches(matches)
     matches.each do |m|
-      match = Match.new
+      match = Match.find_by(match_id: m['id']) || Match.new
       match.match_id = m['id']
       match.season = m['season']
       match.formatted_date = m['formatted_date']
       match.start_time = m['time']
       match.venue_city = m['venue_city']
       match.localteam_name = m['localteam_name']
-      match.localteam_name = m['visitorteam_name']
-      match.localteam_name = m['localteam_score'] if m['localteam_score'].present?
-      match.localteam_name = m['visitorteam_score'] if m['visitorteam_score'].present?
+      match.visitorteam_name = m['visitorteam_name']
+      match.localteam_score = m['localteam_score'] if m['localteam_score'].present?
+      match.visitorteam_score = m['visitorteam_score'] if m['visitorteam_score'].present?
       match.save!
     end
 
